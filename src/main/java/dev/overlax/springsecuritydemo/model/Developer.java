964278937aa9a4295c48feda1d9ae88de536aa4f
alexyekymov <alexyekymov@gmail.com>
2023-05-24 15:02:39 +0300

    init project template

---
+++ src/main/java/dev/overlax/springsecuritydemo/model/Developer.java
@@ -0,0 +1,15 @@
+package dev.overlax.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Developer {
+
+    private Long id;
+    private String firstName;
+    private String last_name;
+}
